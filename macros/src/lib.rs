2668dd6d3ca7682be6ea1ccdd900b278ff4c0365
Viktor Sonesten <v@tmplt.dev>
2021-05-01 08:51:33 +0200

    rtic-trace/macros: drop unnecessary proc-macro2

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,17 +1,13 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use syn::{self, ItemFn, LitInt, Stmt};
 
 static mut TRACE_ID: usize = 0;
 
 #[proc_macro_attribute]
-pub fn trace(attrs: TokenStream, item: TokenStream) -> TokenStream {
-    let _attrs: TokenStream2 = attrs.into();
-    let item: TokenStream2 = item.into();
-
-    let mut fun = syn::parse2::<ItemFn>(item).unwrap();
+pub fn trace(_attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let mut fun = syn::parse::<ItemFn>(item).unwrap();
     fun.block.stmts = {
         let id = syn::parse_str::<LitInt>(
             format!("{}", unsafe {
