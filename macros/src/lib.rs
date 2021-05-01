26c95a1852f533b7382696c5fc6f2bbe32abf754
Viktor Sonesten <v@tmplt.dev>
2021-05-01 06:38:40 +0200

    rtic-trace/macros: add trace stmt at start/end of passed ItemFn

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,15 +1,26 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, ToTokens};
+use syn::{self, ItemFn, Stmt};
 
 #[proc_macro_attribute]
-pub fn trace(_attrs: TokenStream, item: TokenStream) -> TokenStream {
-    // rtic_sw_task_trace::trace(attrs, item)
-    item
+pub fn trace(attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let _attrs: TokenStream2 = attrs.into();
+    let item: TokenStream2 = item.into();
 
+    let mut fun = syn::parse2::<ItemFn>(item).unwrap();
+    fun.block.stmts = {
+        let trace_stmt = syn::parse2::<Stmt>(quote!(
+            ::rtic_trace::set_current_task_id(42);
+        ))
+            .unwrap();
 
-    // let args: TokenStream2 = args.into();
-    // let input: TokenStream2 = input.into();
+        let mut stmts = vec![trace_stmt.clone()];
+        stmts.append(&mut fun.block.stmts);
+        stmts.push(trace_stmt);
+        stmts
+    };
 
-    // TODO copy compile error formatting from rtic-syntax to print what we get here.
-    // We need to check if we can chain proc macros
+    fun.into_token_stream().into()
 }
