bf6cdcffec4955bb9ae62d31de07cc53e4903972
Viktor Sonesten <v@tmplt.dev>
2021-05-01 06:52:07 +0200

    rtic-trace/macros: assign unique ID to each trace item

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,7 +2,9 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
-use syn::{self, ItemFn, Stmt};
+use syn::{self, ItemFn, LitInt, Stmt};
+
+static mut TRACE_ID: usize = 0;
 
 #[proc_macro_attribute]
 pub fn trace(attrs: TokenStream, item: TokenStream) -> TokenStream {
@@ -11,10 +13,19 @@ pub fn trace(attrs: TokenStream, item: TokenStream) -> TokenStream {
 
     let mut fun = syn::parse2::<ItemFn>(item).unwrap();
     fun.block.stmts = {
+        let id = syn::parse_str::<LitInt>(
+            format!("{}", unsafe {
+                let retval = TRACE_ID;
+                TRACE_ID += 1;
+                retval
+            })
+            .as_str(),
+        )
+        .unwrap();
         let trace_stmt = syn::parse2::<Stmt>(quote!(
-            ::rtic_trace::set_current_task_id(42);
+            ::rtic_trace::set_current_task_id(#id);
         ))
-            .unwrap();
+        .unwrap();
 
         let mut stmts = vec![trace_stmt.clone()];
         stmts.append(&mut fun.block.stmts);
