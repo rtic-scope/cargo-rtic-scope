fd9b4502bc0451e80ad9f8e634d8fadce8ac66fe
Viktor Sonesten <v@tmplt.dev>
2021-05-02 05:49:13 +0200

    rtic-trace/macros: document implementation

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -9,6 +9,8 @@ static mut TRACE_ID: usize = 0;
 pub fn trace(_attrs: TokenStream, item: TokenStream) -> TokenStream {
     let mut fun = parse_macro_input!(item as ItemFn);
     fun.block.stmts = {
+        // Generate a unique (software) task ID by strictly increasing a
+        // variable that preserves state over multiple macro calls.
         let task_id = syn::parse_str::<LitInt>(
             format!("{}", unsafe {
                 let retval = TRACE_ID;
@@ -18,11 +20,13 @@ pub fn trace(_attrs: TokenStream, item: TokenStream) -> TokenStream {
             .as_str(),
         )
         .unwrap();
+
+        // Insert a statement at the start and end of the given function
+        // that writes the unique task ID to the watchpoint address.
         let trace_stmt = syn::parse2::<Stmt>(quote!(
             ::rtic_trace::__write_trace_payload(#task_id);
         ))
         .unwrap();
-
         let mut stmts = vec![trace_stmt.clone()];
         stmts.append(&mut fun.block.stmts);
         stmts.push(trace_stmt);
