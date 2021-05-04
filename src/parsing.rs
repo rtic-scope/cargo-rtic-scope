8bbe1d60a6749efc8d554ada1185cecc6530db87
Viktor Sonesten <v@tmplt.dev>
2021-05-04 10:46:53 +0200

    rtic-trace/parsing: cleanup software_tasks

--- src/parsing.rs
+++ src/parsing.rs
@@ -142,38 +142,82 @@ pub fn resolve_int_nrs(
         .collect()
 }
 
-/// Parses an RTIC app and associates the absolute path of the function
-/// that is decorated with [trace], with it's assigned task ID, received
-/// over SWO.
-pub fn software_tasks(app: TokenStream) -> Result<SwAssocs, syn::Error> {
-    // iterate down along scopes (mod, fn), and keep tab on current context
-    // when we find a #[trace], genrate the task id, and assign the absolute function path to it.
+struct TaskIDGenerator(usize);
+impl TaskIDGenerator {
+    pub fn new() -> Self {
+        TaskIDGenerator(0)
+    }
 
-    let app = syn::parse2::<syn::ItemMod>(app)?;
-    let mut ctx: Vec<syn::Ident> = vec![app.ident.clone()];
+    /// Generate a unique task id. Returned values mirror the behavior
+    /// of the `trace`-macro from the tracing module.
+    pub fn generate(&mut self) -> usize {
+        let id = self.0;
+        self.0 += 1;
+        id
+    }
+}
+
+/// Parses an RTIC `mod app` and associates the absolute path of the
+/// functions that are decorated with the `trace`-macro with it's
+/// assigned task ID.
+pub fn software_tasks(app: TokenStream) -> Result<SwAssocs, syn::Error> {
+    let app = syn::parse2::<syn::Item>(app)?;
+    let mut ctx: Vec<syn::Ident> = vec![];
     let mut assocs = SwAssocs::new();
-    let mut task_id: usize = 0;
+    let mut id_gen = TaskIDGenerator::new();
 
     fn traverse_item(
         item: &syn::Item,
         ctx: &mut Vec<syn::Ident>,
         assocs: &mut SwAssocs,
-        task_id: &mut usize,
+        id_gen: &mut TaskIDGenerator,
     ) {
         match item {
+            // handle
+            //
+            //   #[trace]
+            //   fn fun() {
+            //       #[trace]
+            //       fn sub_fun() {
+            //           // ...
+            //       }
+            //   }
+            //
             syn::Item::Fn(fun) => {
+                // record the full path of the function
                 ctx.push(fun.sig.ident.clone());
-                let trace_attr = syn::parse2::<syn::Path>(quote!(trace)).unwrap();
-                if fun.attrs.iter().any(|a| a.path == trace_attr) {
-                    assocs.insert(*task_id, ctx.clone());
-                    *task_id += 1;
+
+                // is the function decorated with #[trace]?
+                if fun.attrs.iter().any(|a| a.path == syn::parse_quote!(trace)) {
+                    assocs.insert(id_gen.generate(), ctx.clone());
                 }
 
+                // walk down all other nested functions
                 for item in fun.block.stmts.iter().filter_map(|stmt| match stmt {
                     syn::Stmt::Item(item) => Some(item),
                     _ => None,
                 }) {
-                    traverse_item(item, ctx, assocs, task_id);
+                    traverse_item(item, ctx, assocs, id_gen);
+                }
+
+                // we've handled with function, return to upper scope
+                ctx.pop();
+            }
+            // handle
+            //
+            //   mod scope {
+            //       #[trace]
+            //       fn fun() {
+            //           // ...
+            //       }
+            //   }
+            //
+            syn::Item::Mod(m) => {
+                ctx.push(m.ident.clone());
+                if let Some((_, items)) = &m.content {
+                    for item in items {
+                        traverse_item(&item, ctx, assocs, id_gen);
+                    }
                 }
                 ctx.pop();
             }
@@ -181,10 +225,7 @@ pub fn software_tasks(app: TokenStream) -> Result<SwAssocs, syn::Error> {
         }
     }
 
-    let (_, items) = app.content.unwrap();
-    for item in items {
-        traverse_item(&item, &mut ctx, &mut assocs, &mut task_id);
-    }
+    traverse_item(&app, &mut ctx, &mut assocs, &mut id_gen);
 
     Ok(assocs)
 }
