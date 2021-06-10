78ee04f039e5778639e97f965f263cda22003214
Viktor Sonesten <v@tmplt.dev>
2021-06-10 19:00:49 +0200

    refactor: merge rtic_trace::parsing into cargo-rtic-trace
    rtic_trace::parsing is a non-trivial feature-set currently only in use
    by cargo-rtic-trace. To mix it with a no-std feature-set and make the
    two sets mutually exclusive is just asking for trouble. In the end we
    want to minimize the overhead of the end-user. If a crate must then be
    included it should not require any features.
    
    If needed, the parsing module can later be split out into some
    cortex-m-rtic-trace-syntax crate.

--- assets/libadhoc/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#![allow(improper_ctypes_definitions)]
-use cortex_m::interrupt::Nr;
-
-// purposefully left empty
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#![cfg_attr(feature = "tracing", no_std)]
+#![no_std]
 //! This crate exposes functionality that eases the procedure of tracing
-//! embedded applications written using RTIC. Two mutually exclusive
-//! feature-sets (modules, locked behind crate features) are exposed:
+//! embedded applications written using RTIC. A single feature-set is
+//! available:
 //!
 //! - `tracing`: which offers setup functions that configures related
 //!   peripherals for RTIC task tracing, and a `#[trace]` macro for
@@ -42,24 +42,5 @@
 //!       }
 //!   }
 //!   ```
-//!
-//! - `parsing`: which offers functions that parses an RTIC application
-//!   source `#[app(device = ...)] mod app { ... }` declaration,
-//!   recovers type informations, and associates the trace packets
-//!   received from the target back to RTIC tasks. For use on the host
-//!   system that receives the trace information from the embedded
-//!   target.
-//!
-//! Refer to the generated documentation when this crate is built with
-//! the `tracing` or `parsing` feature. Because `tracing` is a
-//! `#[no_std]` module, and `parsing` required `std`, this crate does
-//! nothing by default.
 
-#[cfg(all(feature = "tracing", feature = "parsing"))]
-compile_error!("Crate features \"tracing\" and \"parsing\" are mutually exclusive");
-
-#[cfg(all(feature = "parsing", not(feature = "tracing")))]
-pub mod parsing;
-
-#[cfg(all(feature = "tracing", not(feature = "parsing")))]
 pub mod tracing;
