9cfd347a21ff4560a854e170c5ab59f900c33ab3
Viktor Sonesten <v@tmplt.dev>
2021-06-14 12:47:09 +0200

    building: correctly evade .cargo/config when building libadhoc

--- cargo-rtic-trace/src/building.rs
+++ cargo-rtic-trace/src/building.rs
@@ -14,8 +14,6 @@ use cargo_metadata::{Artifact, Message};
 /// Ad-hoc build of target binary. Adapted from
 /// <https://github.com/rust-embedded/cargo-binutils/blob/115e26e7640337450b609d0d1d14619a1c370c7a/src/lib.rs#L402>.
 pub fn cargo_build(crate_directory: &Path, args: &[&str], kind: &str) -> Result<Artifact> {
-    env::set_current_dir(crate_directory)?;
-
     let cargo = env::var_os("CARGO").unwrap_or_else(|| "cargo".into());
     let mut cargo = Command::new(cargo);
     cargo.arg("build");
@@ -23,6 +21,19 @@ pub fn cargo_build(crate_directory: &Path, args: &[&str], kind: &str) -> Result<
 
     cargo.arg("--message-format=json");
     cargo.stdout(Stdio::piped());
+
+    // Dirty fix for evading any eventual .cargo/config in the working
+    // directory. We obviously need it when we build the target
+    // application, but it breaks libadhoc build.
+    if kind == "cdylib" {
+        env::set_current_dir(env::temp_dir())?;
+        cargo.args(&[
+            "--manifest-path",
+            crate_directory.join("Cargo.toml").to_str().unwrap(),
+        ]);
+    } else {
+        assert_eq!(env::current_dir()?, crate_directory);
+    }
 
     eprintln!("{:?}", cargo);
 
