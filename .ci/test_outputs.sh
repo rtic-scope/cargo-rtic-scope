4d19aa5578f60a88525a7d3419f14ff0f9d8ec40
Viktor Sonesten <v@tmplt.dev>
2021-10-31 10:28:46 +0100

    ci/resolve: test some more hint outputs

--- .ci/test_outputs.sh
+++ .ci/test_outputs.sh
@@ -7,6 +7,7 @@ pushd $(dirname "$0")/expected >/dev/null
 
 # For each --bin, `trace --resolve-only` it, and compare with expected
 # output.
+cp ./manifests/general.toml Cargo.toml
 for bin in src/bin/*.rs; do
     bin=$(basename "$bin" .rs)
     out=$($rtic_scope trace --resolve-only --bin $bin 2>&1 || true)
@@ -15,6 +16,18 @@ for bin in src/bin/*.rs; do
     while read line; do
         echo "$out" | grep -F "$line" >/dev/null || exit 1
     done < ./out/$bin.run
+done
+
+# Same as above, but for each manifest with general.rs
+for manifest in manifests/*.toml; do
+    cp $manifest Cargo.toml
+    manifest=$(basename "$manifest" .toml)
+    out=$($rtic_scope trace --resolve-only --bin general 2>&1 || true)
+
+    # for each (fixed) expected string, ensure it's in the output.
+    while read line; do
+        echo "$out" | grep -F "$line" >/dev/null || exit 1
+    done < ./out/$manifest.run
 done
 
 popd >/dev/null
